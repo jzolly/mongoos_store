de54fbf21ebc41c13c592e0d4b742c08a49f8c30
Jessie Zollinger <jzolly@hotmail.com>
2022-05-25 11:15:03 -0400

    added edit and delete functionality

--- server.js
+++ server.js
@@ -2,9 +2,9 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
-
 const mongoose = require('mongoose');
-const Product = require('./models/products')
+const Product = require('./models/products');
+const methodOverride = require('method-override');
 // Database Connections
 mongoose.connect(process.env.DATABASE_URL, {
 	useNewUrlParser: true,
@@ -20,6 +20,7 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 
 // MIDDLEWARE
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 // connect CSS
 app.use(express.static('public'));
 
@@ -47,15 +48,44 @@ app.get('/products', (req, res) => {
 app.get('/products/new', (req, res) => {
 	res.render('new.ejs');
 })
-// D
+// Delete
+app.delete('/products/:id', (req, res) => {
+	Product.findByIdAndRemove(req.params.id, (err, data) => {
+		res.redirect('/products')
+	});
+});
+
+
 // U
+app.put("/products/:id", (req, res) => {
+	Product.findByIdAndUpdate(
+		req.params.id,
+		req.body, 
+		{
+			new: true,  //allows us to receive a new version of the document
+		},
+		(error, updateProduct) => {
+			res.redirect(`/products/${req.params.id}`) //shows you how to redirect to where you are working.
+		}
+	)
+  });
+  
 // Create
 app.post('/products', (req, res) => {
     Product.create(req.body, (error, createdProduct) => {
 		res.redirect('/products');
 	});
 });
+
 // E
+app.get('/products/:id/edit', (req, res) => {
+	Product.findById(req.params.id, (error, foundProduct) => {
+		res.render('edit.ejs', {
+			product: foundProduct,
+		});
+	});
+});
+
 // Show
 app.get('/products/:id', (req, res) => {
 	Product.findById(req.params.id, (err, foundProduct) => {
