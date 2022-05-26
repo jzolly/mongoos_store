bbab90276e517787d18b461281661a878e6789ec
Jessie Zollinger <jzolly@hotmail.com>
2022-05-25 20:52:00 -0400

    add to cart button

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ app.use(express.static('public'));
 const productSeed = require('./models/productSeed');
 
 app.get('/products/seed', (req, res) => {
-	Product.deleteMany({}, (error, allProducts) => {});
-	
+	Product.deleteMany({}, (error, allProducts) => { });
+
 	Product.create(productSeed, (error, data) => {
 		res.redirect('/products');
 	})
@@ -38,10 +38,32 @@ app.get('/products', (req, res) => {
 	})
 });
 
+//CART STUFF
+app.get('/cart', (req, res) => {
+	res.render('cart.ejs')
+})
+
+app.post('/cart', (req, res) => {
+	const productId = req.body.productId;
+	Product.findByIdAndUpdate(
+		productId,
+		{ $inc: { qty: -1 } },
+		{
+			new: true,  //allows us to receive a new version of the document
+		},
+		(error, updatedProduct) => {
+			console.error(error);
+			res.redirect(`/products/${productId}`) //shows you how to redirect to where you are working.
+		}
+	)
+})
+
+
 // N
 app.get('/products/new', (req, res) => {
 	res.render('new.ejs');
 })
+
 // Delete
 app.delete('/products/:id', (req, res) => {
 	Product.findByIdAndRemove(req.params.id, (err, data) => {
@@ -49,54 +71,60 @@ app.delete('/products/:id', (req, res) => {
 	});
 });
 
-
-// U
+// Update
 app.put("/products/:id", (req, res) => {
 	Product.findByIdAndUpdate(
 		req.params.id,
-		req.body, 
+		req.body,
 		{
 			new: true,  //allows us to receive a new version of the document
 		},
-		(error, updateProduct) => {
+		(error, updatedProduct) => {
 			res.redirect(`/products/${req.params.id}`) //shows you how to redirect to where you are working.
 		}
-		)
+	)
+});
+
+
+
+// Cart/BUY Button
+app.put('/products/:id', (req, res) => {
+
+})
+
+// Create
+app.post('/products', (req, res) => {
+	Product.create(req.body, (error, createdProduct) => {
+		res.redirect('/products');
 	});
-	
-	// Create
-	app.post('/products', (req, res) => {
-		Product.create(req.body, (error, createdProduct) => {
-			res.redirect('/products');
+});
+// E
+app.get('/products/:id/edit', (req, res) => {
+	Product.findById(req.params.id, (error, foundProduct) => {
+		res.render('edit.ejs', {
+			product: foundProduct,
 		});
 	});
-	
-	// E
-	app.get('/products/:id/edit', (req, res) => {
-		Product.findById(req.params.id, (error, foundProduct) => {
-			res.render('edit.ejs', {
-				product: foundProduct,
-			});
+});
+
+// Show
+app.get('/products/:id', (req, res) => {
+	Product.findById(req.params.id, (err, foundProduct) => {
+		res.render('show.ejs', {
+			product: foundProduct,
 		});
 	});
-	
-	// Show
-	app.get('/products/:id', (req, res) => {
-		Product.findById(req.params.id, (err, foundProduct) => {
-			res.render('show.ejs', {
-				product: foundProduct,
-			});
-		});
-	});
-	
-	// DB CONNECTIONS Error/Success
-	// Define callback functions for various events
-	const db = mongoose.connection
-	db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
-	db.on('connected', () => console.log('mongo connected'));
-	db.on('disconnected', () => console.log('mongo disconnected'));
-	
-	// Listeners
-	const PORT = process.env.PORT;
+});
+
+// DB CONNECTIONS Error/Success
+// Define callback functions for various events
+const db = mongoose.connection
+db.on('error', (err) => console.log(err.message + ' is mongo not running?'));
+db.on('connected', () => console.log('mongo connected'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Listeners
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`server is listening on port: ${PORT}`));
+
 
